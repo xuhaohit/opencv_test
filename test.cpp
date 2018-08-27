a76785471565d420450643842e7c3e23c815898a
haoxu <zgkdxuhao@163.com>
2018-08-27 11:06:17 +0800

    delete worthless info

--- test.cpp
+++ test.cpp
@@ -7,19 +7,9 @@ int main(int argc, char** argv )
     std::cout<<"!111"<<std::endl;
 :qw
 
-    //VideoCapture capture(0);
     Mat image;
-    /*while(true){
-     
-      capture>>image;
-
-      
-   }*/
-    //  /home/sima/data/drive_area/seg_gt/0000000000_idx.png
-    image = imread( "../1.png",-1);
     std::cout<<image.channels()<<std::endl;
     imshow("Display Image", image);
-    image.
     waitKey(0);
     return 0;
 }
